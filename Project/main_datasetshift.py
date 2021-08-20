74848f2e8610556768790f5f205c4b36fbd727ae
ga96tum <ga96tum@mytum.de>
2021-08-20 11:54:23 +0200

    new plotting

--- Project/main_datasetshift.py
+++ Project/main_datasetshift.py
@@ -27,7 +27,7 @@ get_data= datapreprocessing.Dataprep(0,0,0,0,0,0,test_size)
 #print(get_data.target_test.shape)
 
 ####with datasetshift
-get_data.dataset_shift(369)#41,243,394,433
+get_data.dataset_shift(369)#41,243,394,433,369
 #print(get_data.inputs_test.shape)
 #print(get_data.inputs_training.shape)
 #print(get_data.target_training.shape)
